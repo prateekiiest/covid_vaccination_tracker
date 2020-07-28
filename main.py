1bf477ffe9a6b467187bcf11ee7ec1aaa9b22692
dilanbhalla <dbhalla21@berkeley.edu>
2020-07-27 17:49:49 -0700

    improving code readability

--- main.py
+++ main.py
@@ -10,60 +10,59 @@
 # 9. Coming in contact with someone who has been diagnosed with Covid 19: N
 # 10. Do you have any kind of Covid 19 symptoms: N
 
+import random, requests
 import numpy as np
-import random
 import pandas as pd
-import requests
 from bs4 import BeautifulSoup
 
 
-# Get pincodes
-pincodes = []
-
-def is_int(s):
-    try: 
-        int(s)
-        return True
-    except ValueError:
-        return False
-
-url = requests.get("https://chennaiiq.com/india/pincode/index.asp?id=16&state_name=Karnataka&page=B").text
-soup = BeautifulSoup(url, 'lxml')
-trs = soup.find_all('tr', class_="tab")
-
-for tr in trs:
-    for td in tr.find_all('td'):
-        value = str(td).replace("<td>", "").replace("</td>", "")
-        if is_int(value):
-            if int(value) > 100000:
-                pincodes.append(int(value))
-
-population = 5
+# Set population, initialize dataset
+population = 100
 data = np.empty((0,10), str)
-features = ['UUID', 'Gender', 'Age', 'Profession', 'Locality/Pin Code', 'Rate of infection in that zone', 'Pre-existing medical conditions', 'Travel history in the last 1 month', 'Coming in contact with someone who has been diagnosed with Covid 19', 'Do you have any kind of Covid 19 symptoms']
 
-uuids = random.sample(range(0, population), population)
 genders = ["M", "F"]
 ages = np.arange(1, 71, 1).tolist()
 professions = ["Health Care", "Security", "Police", "Blue Collar worker", "Work from Home", "Public Policy", "Bank"]
 conditions = ["None", "Diabetes", "Cancer", "Kidney Ailments", "Pulmonary", "Heart"]
 yes_no_questions = ["Y", "N"]
 
-for i in range(population):
+# Scrape site for KARNATAKA pincodes
+def generate_pincodes():
+    def is_int(s):
+        try: 
+            int(s)
+            return True
+        except ValueError:
+            return False
+    pincodes = []
+    url = requests.get("https://chennaiiq.com/india/pincode/index.asp?id=16&state_name=Karnataka&page=B").text
+    soup = BeautifulSoup(url, 'lxml')
+    trs = soup.find_all('tr', class_="tab")
+    for tr in trs:
+        for td in tr.find_all('td'):
+            value = str(td).replace("<td>", "").replace("</td>", "")
+            if is_int(value):
+                if int(value) > 100000:
+                    pincodes.append(int(value))
+    return pincodes
 
-    person = np.array([])
-
+# Generate random values for each of the features for a given person
+def generate_random_features():
     uuid = str(np.random.randint(1000, 9999)) + "-" + str(np.random.randint(1000, 9999)) + "-" + str(np.random.randint(1000, 9999)) + "-" + str(np.random.randint(1000, 9999))
     gender = random.choice(genders)
     age = random.choice(ages)
     profession = random.choice(professions)
-    pincode = random.choice(pincodes)
+    pincode = random.choice(generate_pincodes())
     infection_rate = round(random.uniform(0.8, 1.2), 1)
     condition = random.choice(conditions)
     travel = random.choice(yes_no_questions)
     contact = random.choice(yes_no_questions)
     symptoms = random.choice(yes_no_questions)
+    return (uuid, gender, age, profession, pincode, infection_rate, condition, travel, contact, symptoms)
 
+def create_person(uuid, gender, age, profession, pincode, infection_rate, condition, travel, contact, symptoms):
+    person = np.array([])
+    # Add features to person array
     person = np.append(person, uuid)
     person = np.append(person, gender)
     person = np.append(person, age)
@@ -74,9 +73,30 @@ for i in range(population):
     person = np.append(person, travel)
     person = np.append(person, contact)
     person = np.append(person, symptoms)
+    return person
 
-    data = np.append(data, [person], axis=0)
+# Build dataset
+def build_dataset():
+    for _ in range(population):
+        # Will be manipulating global dataset
+        global data
+        # Generate random features for each person
+        features = generate_random_features()
+        uuid, gender, age, profession, pincode, infection_rate, condition, travel, contact, symptoms = features[0], features[1], features[2], features[3], features[4], features[5], features[6], features[7], features[8], features[9] 
+        # Add features to person array, add person to data array
+        person = create_person(uuid, gender, age, profession, pincode, infection_rate, condition, travel, contact, symptoms)
+        data = np.append(data, [person], axis=0)
 
-df = pd.DataFrame(data, columns=features)
-pd.DataFrame(data, columns=features).to_csv("data.csv", index=False)
-print(df.to_string(index=False))
+if __name__ == '__main__':
+
+    # initialize dataset and define columns
+    features_list = ['UUID', 'Gender', 'Age', 'Profession', 'Locality/Pin Code', 'Rate of infection in that zone', 'Pre-existing medical conditions', 'Travel history in the last 1 month', 'Coming in contact with someone who has been diagnosed with Covid 19', 'Do you have any kind of Covid 19 symptoms']
+
+    # poppulate data array
+    build_dataset()
+    # Export data
+    df = pd.DataFrame(data, columns=features_list)
+    pd.DataFrame(data, columns=features_list).to_csv("data.csv", index=False)
+
+    # Only for testing, do not print for large data sets
+    # print(df.to_string(index=False))
